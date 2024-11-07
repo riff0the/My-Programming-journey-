62a6f41b3829c7e9c4db9f1dcd684010c8c429ef
riff0the <rifkiseptiadi044@gmail.com>
2024-11-07 10:20:43 +0700

    PROGRAM PUSH BUTTON SERIAL MONITOR
    ketika ditekan tombol muncul text "iya" ketika tidak diekan tombolnya maka muncul text "tidak"

---
+++ LKPD_10.ino
@@ -0,0 +1,13 @@
+const int buttonPin = 26;
+void setup(){
+  pinMode(buttonPin,INPUT);
+  Serial.begin(9600);}
+  void loop(){
+    int buttonState=digitalRead(buttonPin);
+    Serial.print("Tombol ditekan:");
+    if (buttonState==HIGH){
+      Serial.println("YA");
+    }else{
+      Serial.print("tidak");}
+      delay(100);}
+    
