caa489b2bc62f8dc46304c7a0aa5a86650d4885e
riff0the <rifkiseptiadi044@gmail.com>
2024-11-28 12:18:08 +0700

    PROGRAM BMP280 SERIAL MONITOR
    saat program dijalankan trainer akan membaca nilai MQ@ dan muncul di serial monitor

---
+++ LKPD_14.ino
@@ -0,0 +1,12 @@
+
+ //Rifki septiadi
+ //XI TAV1
+#define MQ2_PIN 4
+void setup() {
+  Serial.begin(115200);
+  pinMode(MQ2_PIN,  INPUT);}
+  void loop() {
+    int mq2Value = analogRead(MQ2_PIN);
+    Serial.print("Nilai MQ2-2: ");
+    Serial.println(mq2Value);
+    delay(1000);}
